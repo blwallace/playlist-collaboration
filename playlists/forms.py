2f561bd4042fde45bf350d1140a57b4b0adf95d2
blwallace <blwallace@gmail.com>
2016-08-22 11:56:18 -0400

    select playlist

--- playlists/forms.py
+++ playlists/forms.py
@@ -4,9 +4,11 @@ from .models import Song, Playlist
 
 class SongForm(forms.ModelForm):
 
+    playlist = forms.ModelChoiceField(queryset=Playlist.objects.all())
+
     class Meta:
         model = Song
-        fields = ('uri','name',)
+        fields = ('uri','name','playlist')
 
 class PlaylistForm(forms.ModelForm):
 
